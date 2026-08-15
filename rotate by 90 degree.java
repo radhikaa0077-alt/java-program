39b804352d88f35ad55aea612a42854d150f997b
jyothi kumari <radhikaa0077@gmail.com>
2026-08-15 05:42:55 +0530

    Add rotate method for 2D array
    Implemented a method to rotate a 2D array by 90 degrees clockwise.

---
+++ rotate by 90 degree.java
@@ -0,0 +1,23 @@
+class gfg{
+  static void rotate(int arr[]){
+    //transpose
+  for(int i=0; i<arr.length; i++){
+    for(int j=0; j<i; j++){
+      int temp = arr[i][j];
+      arr[i][j]= arr[j][i];
+      arr[j][i] = temp;
+    }
+  }
+    //reverse each row
+     for(int i=0; i<arr.length; i++){
+       int stcol=0 endcol=arr[0].length-1;
+       while(stcol<endcol){
+         int temp =arr[i][stcol];
+         arr[i][stcol]=arr[i][endcol];
+         arr[i][endcol]=temp;
+         stcol++;
+         end--;
+       }
+     }
+  }
+}  
