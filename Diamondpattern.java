e38899f93e179fa67df946d1382a3c96586d0127
jyothi kumari <radhikaa0077@gmail.com>
2026-08-26 21:19:54 +0530

    Add Diamondpattern.java for diamond shape output
    Implement a Java program to print a diamond pattern.

---
+++ Diamondpattern.java
@@ -0,0 +1,28 @@
+public class Diamondpattern{
+  public static void main(string[]args){
+    int n=4;
+    //upper half
+    for(int i=1; i<=n; i++){
+      //spaces
+    for(int j=1; j<=n-i; j++){
+      System.out.print(" ");
+    }
+      //stars
+    for(int j=1; j<=2*i-1; j++){
+      System.out.print("*");
+    }
+      //lower half
+    for(int i=n; i>=1; i--){
+      //spaces
+    for(int j=1; j<=n-i; j++){
+      System.out.print(" ");
+    }
+      //stars
+    for(int j=1; j<=2*i-1; j++){
+      System.out.print("*");
+    }
+      System.out.println();
+    }
+    }
+  }
+}
